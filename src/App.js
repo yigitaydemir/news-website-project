e81f3c4c0b085c191303041bad638771a142c4bf
Yusuf Yiğit Aydemir <yusufyigitaydemir@Yusuf-MacBook-Air.local>
2022-10-20 11:41:06 +0300

    devam

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import NavbarComp from "./components/NavbarComp";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="container">
-      <div className='card'>
-        <div className='card-body'>
-          <button className='btn btn-primary'>Click Me</button>
-        </div>
-      </div>
+    <div>
+      <NavbarComp></NavbarComp>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/contact" element={<Contact></Contact>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/NavbarComp.js
@@ -0,0 +1,41 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+
+const NavbarComp = () => {
+  return (
+    <div className='container'>
+    <Navbar bg="white" expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="/">Bootstrap Project</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Link to="/" className='nav-link active'>Home</Link>
+            <Link to="/about" className='nav-link active'>About</Link>
+            <Link to="/contact" className='nav-link active'>Contact</Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </div>
+  );
+}
+
+export default NavbarComp;
