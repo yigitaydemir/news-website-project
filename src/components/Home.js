6540f94bff6b736dc5d9c141b4ce6adcc584b28d
yigitaydemir <yusufyigitaydemir@gmail.com>
2022-10-20 17:35:37 +0300

    devam

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from "react";
+import Slider from "./Slider";
 
 const Home = () => {
   return (
-    <div className='container'>
-        <div className='card mt-4'>
-            <div className='card-body'>
-                <h2>Home Page</h2>
-            </div>
+    <div className="container">
+      <Slider></Slider>
+      <div className="card mt-4">
+        <div className="card-body">
+          <h2>Home Page</h2>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
---
+++ src/components/Slider.js
@@ -0,0 +1,50 @@
+import Carousel from 'react-bootstrap/Carousel';
+import slider1 from "../media/slider1.jpg"
+import slider2 from "../media/slider2.webp"
+import slider3 from "../media/slider3.webp"
+
+function Slider() {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={slider1}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={slider2}
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={slider3}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default Slider;
