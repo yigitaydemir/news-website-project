260d95be1a2471bc3733037344ec70ca9fbffbe4
yigitaydemir <yusufyigitaydemir@gmail.com>
2022-10-30 12:56:02 +0300

    devam

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../scroll.css";
+import axios from "axios";
+//hsZE1DZXZYucq5xvmC5uM6Tqz0B1NU6K
 
 const Scroll = () => {
+  useEffect(() => {
+    //fetchNews();
+  }, []);
+
+  const [news, setNews] = useState([]);
+
+  const fetchNews = async () => {
+    const data = await fetch(
+      `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${process.env.REACT_APP_API_KEY}`
+    );
+
+    const news = await data.json();
+    console.log(news.articles);
+    setNews(news.articles);
+  };
+
+  const myHeaders = new Headers();
+myHeaders.append("apikey", "hsZE1DZXZYucq5xvmC5uM6Tqz0B1NU6K");
+
+const requestOptions = {
+  method: 'GET',
+  redirect: 'follow',
+  headers: myHeaders
+};
+
+fetch("https://api.apilayer.com/exchangerates_data/latest?symbols=AED,AUD,CAD,CHF,CNY,EUR,GBP,JPY,TRY&base=USD", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+
+  
   return (
     <div className="marquee">
       <ul className="marquee-content">
